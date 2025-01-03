1e879c2ff0daf63eb84b0ae4b8eb7108d9a0c442
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 12:19:28 +0000

    1.0

--- exercises/026-computeAreaOfATriangle/app.js
+++ exercises/026-computeAreaOfATriangle/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function computeAreaOfATriangle(base, altura) {
+    return area = (base * altura) / 2;
+}
+
+let output = computeAreaOfATriangle(4, 6);
+console.log(output);
