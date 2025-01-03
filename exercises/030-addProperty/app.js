7979941dbdf1625fd61cb1a518375c256dd1338c
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 13:10:17 +0000

    1.0

--- exercises/030-addProperty/app.js
+++ exercises/030-addProperty/app.js
@@ -1,4 +1,17 @@
 function addProperty(obj, key) {
   // your code here
-  
+   obj[key]= true;
+   return obj;
 }
+
+
+let jugador = {
+  nombre : 'Luka Döncic',
+  posicion: 'Base',
+  edad: 25,
+  anillos : false,
+  ppg : 29.3
+};
+
+addProperty(jugador, 'rpg');
+console.log(jugador.rpg);
