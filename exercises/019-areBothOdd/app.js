4b61261ff6c042318d69ec339a48f6a73eeeeb3d
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 11:58:19 +0000

    1.0

--- exercises/019-areBothOdd/app.js
+++ exercises/019-areBothOdd/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function areBothOdd(numero1, numero2) {
+    if (numero1 % 2 != 0 && numero2 % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+let output = areBothOdd(1, 3);
+console.log(output)
--- exercises/020-isEitherEven/app.js
+++ exercises/020-isEitherEven/app.js
@@ -1,1 +1,10 @@
-// Write your function here
+function isEitherEven(num1, num2) {
+    // Write your function here
+    if (num1 % 2 === 0 || num2 % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+let output = isEitherEven(1, 4);
+console.log(output);
