50a6ea1179f658ee6249f18ab88bd69cd7f4a976
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 10:50:46 +0000

    1.0

--- exercises/009-getLengthOfThreeWords/app.js
+++ exercises/009-getLengthOfThreeWords/app.js
@@ -1,6 +1,12 @@
 function getLengthOfThreeWords(word1, word2, word3) {
   // your code here
-  
+  let sumatoria = 0;
+sumatoria = word1.length + sumatoria;
+sumatoria = word2.length + sumatoria;
+sumatoria = word3.length + sumatoria;
+
+return sumatoria;
+
 }
 
 let output = getLengthOfThreeWords('some', 'other', 'words');
