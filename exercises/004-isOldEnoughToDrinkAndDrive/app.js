6919a2be54f794250fc99c22744fba907cc17049
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 10:30:08 +0000

    1.0

--- exercises/004-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/004-isOldEnoughToDrinkAndDrive/app.js
@@ -1,4 +1,14 @@
 function isOldEnoughToDrinkAndDrive(age) {
-  // your code here
+  if (age >= 16 && age < 21) {
+    return false;
+  } else if (age >= 21) {
+    return false;
+  } else{
+    return false;
+  }
   
 }
+
+
+let output = isOldEnoughToDrinkAndDrive(22);
+console.log(output);
--- exercises/005-checkAge/app.js
+++ exercises/005-checkAge/app.js
@@ -1,4 +1,14 @@
 function checkAge(name, age) {
-  // your code here
+  if (age < 21 && age >= 0) {
+    return `Go home, ${name}!`;
+  } else if (age >= 21 && age < 135) {
+    return `Welcome, ${name}!`;
+  } else {
+    return "Error";
+  }
   
 }
+
+let output = checkAge('Adrian', 22);
+console.log(output);
+
