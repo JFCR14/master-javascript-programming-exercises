2c7d0357b5b053dae8cca976d75c97768e50d931
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 09:55:03 +0000

    1.0

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,4 +1,14 @@
 function isOldEnoughToDrink(age) {
-    // your code here
+    if (age < 21) {
+        return false;
+    } else if(age >= 21){
+        return true;
+    } else {
+        return console.log("Error");
+    }
     
 }
+
+let output = isOldEnoughToDrink(19);
+console.log(output); 
+
