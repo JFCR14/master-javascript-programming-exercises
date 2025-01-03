c87136be8eb652f236e1b373677e76c6a09c47de
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 11:38:49 +0000

    1.0

--- exercises/018-isOdd/app.js
+++ exercises/018-isOdd/app.js
@@ -1,1 +1,13 @@
 // Write your function here
+// Write your function here
+
+function isOdd(num) {
+    if (num % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let output = isOdd(11);
+console.log(output);
