ffe997f7ef59d40213abca095c2ff73d2e6def1f
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-06 14:34:09 +0000

    1.0

--- exercises/063-getAllLetters/app.js
+++ exercises/063-getAllLetters/app.js
@@ -1,7 +1,11 @@
 function getAllLetters(str) {
-    // your code here
+    let array = []
+    for (let i = 0; i < str.length; i++) {
+        array[i] = str[i];
+    }
+    return array;
     
 }
 
-let output = getAllLetters('Radagast');
+let output = getAllLetters('');
 console.log(output); // --> ['R', 'a', 'd', 'a', 'g', 'a', 's', 't']
