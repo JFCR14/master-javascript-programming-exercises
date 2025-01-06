1fe3250a075a5bce1dde7c7fb0cfb4679d32feb2
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-06 13:49:30 +0000

    1.0

--- exercises/057-getElementsUpTo/app.js
+++ exercises/057-getElementsUpTo/app.js
@@ -1,7 +1,10 @@
 function getElementsUpTo(array, n) {
-  // your code here
-  
+  let arr = array.slice(0, n );
+  return arr;  
 }
 
 let output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
 console.log(output); // --> ['a', 'b', 'c']
+
+let output2 = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output2); // --> ['a', 'b', 'c']
