0d0ecf374957cb26f42148e4c844cd362ffc2c83
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-06 00:49:14 +0000

    1.0

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,22 @@
 // Write your function here
+
+function computeAverageOfNumbers(array) {
+    if (array.length === 0) {
+        return 0; 
+    } else {
+        let suma = 0; 
+        for (let i = 0; i < array.length; i++) {
+            suma += array[i]; 
+        }
+        let promedio = suma / array.length; 
+        return promedio; 
+    }
+}
+
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); 
+
+let input2 = [];
+let output2 = computeAverageOfNumbers(input2);
+console.log(output2); 
