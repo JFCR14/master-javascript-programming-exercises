9320872596770873e578d10a2c659e7e8d0c9486
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-06 16:47:05 +0000

    1.0

--- exercises/066-extend/app.js
+++ exercises/066-extend/app.js
@@ -8,6 +8,15 @@ let obj2 = {
 };
 
 function extend(obj1, obj2) {
-    
+    for (let key in obj2) {
+        if (!(key in obj1)) { 
+            obj1[key] = obj2[key]; 
+        }
+    }
 
 }
+
+extend(obj1, obj2);
+
+console.log(obj1); // --> {a: 1, b: 2, c: 3}
+console.log(obj2); // --> {b: 4, c: 3}
