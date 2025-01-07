3de73b9803cd56e3100511987f879c5146e92577
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-07 21:56:31 +0000

    1.0

--- exercises/072-removeOddValues/app.js
+++ exercises/072-removeOddValues/app.js
@@ -1,5 +1,10 @@
 function removeOddValues(obj) {
-    // your code here
+    for (const key in obj) {
+      if (typeof obj[key] === "number" && obj[key]%2 != 0) {
+        delete obj[key];
+        
+      }
+    }
 
 }
 
